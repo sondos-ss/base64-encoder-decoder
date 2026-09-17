b1d399bff98dad03fc99dd14bf30ac2fc78f1202
sondos--ss <sondos.said47@gmail.com>
2026-09-17 20:50:48 +0300

    initail commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// b64_size stores, for an input of n bytes, the character count in chars and
+// the number of '=' characters in pads.
+//
+// TODO: chars = 4 characters for every 3-byte group, with the last partial
+// group rounded UP to a whole group; pads = 0, 2 or 1, decided by n % 3.
+// C++ hint: / on long long is already integer division and % is the
+// remainder, so no floating point is needed.
+static void b64_size(long long n, long long &chars, long long &pads) {
+    (void)n;
+    chars = 4;
+    pads = 2;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    string line;
+    while (getline(cin, line)) {
+        size_t a = line.find_first_not_of(" \t\r\n");
+        if (a == string::npos) continue;
+        size_t b = line.find_last_not_of(" \t\r\n");
+        long long n = atoll(line.substr(a, b - a + 1).c_str());
+        long long chars = 0, pads = 0;
+        b64_size(n, chars, pads);
+        cout << chars << " " << pads << "\n";
+    }
+    return 0;
+}
